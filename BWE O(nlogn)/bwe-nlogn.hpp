6bd7d72df26e0aac39ac7ed6e587b305a73dcb35
soniap13 <sonia.pawluk13@gmail.com>
2020-05-25 15:22:34 +0200

    Implement encode function; Ignore test text files and compiled version of program

--- BWE O(nlogn)/bwe-nlogn.hpp
+++ BWE O(nlogn)/bwe-nlogn.hpp
@@ -8,9 +8,9 @@ class bwe{
 
     public:
         //reads block_size bytes or less if eof was encouterd while reading input file and safes them into list sequence. Returns true if we should continue reading or false if we read whole file
-        static bool read_block(std::list<uint8_t> &sequence, std::ifstream input);
-        static std::list<uint8_t> bwt(std::list<uint8_t> &input_sequence);
-        static std::list<uint8_t> move_to_front(std::list<uint8_t> &input_sequence);
-        static std::list<uint8_t> huffman_encode(std::list<uint8_t> &input_sequence);
-        static std::list<uint8_t> save_to_file(std::ofstream output);
+        static bool read_block(std::list<uint8_t> &input_sequence, std::ifstream &input);
+        static std::list<uint8_t> bwt(std::list<uint8_t> &sequence);
+        static std::list<uint8_t> move_to_front(std::list<uint8_t> &sequence);
+        static std::list<uint8_t> huffman_encode(std::list<uint8_t> &isequence);
+        static void save_to_file(std::list<uint8_t> &output_sequence, std::ofstream &output);
 };
--- BWE O(nlogn)/bwe-nlogn_tools.cpp
+++ BWE O(nlogn)/bwe-nlogn_tools.cpp
@@ -1,31 +1,53 @@
 #include "bwe-nlogn.hpp"
 #include <iostream>
+#include <errno.h>
+#include <string.h>
 using namespace std;
 
+#define ERROR(str) { fprintf(stderr, "%s: %s\n", str, strerror(errno)); exit(EXIT_FAILURE); }
+
 void bwe::encode(char * input_file, char * output_file, int block_size){
+    ifstream input(input_file);
+    if(!input.good())
+        ERROR(input_file);
+    ofstream output(output_file);
+    if(!output.good())
+        ERROR(output_file);
 
+    list<uint8_t> block;
+    bool keep_goin = true;
+
+    while(keep_goin){
+        keep_goin = read_block(block, input);
+        block = bwt(block);
+        block = move_to_front(block);
+        block = huffman_encode(block);
+        save_to_file(block, output);
+    }
+
+    input.close();
+    output.close();
 }
 
-bool bwe::read_block(list<uint8_t> &sequence, ifstream input){
-    return true;
+bool bwe::read_block(list<uint8_t> &input_sequence, ifstream &input){
+    return false;
 }
 
-list<uint8_t> bwe::bwt(list<uint8_t> &input_sequence){
+list<uint8_t> bwe::bwt(list<uint8_t> &sequence){
     list<uint8_t> result;
     return result;
 }
 
-list<uint8_t> bwe::move_to_front(list<uint8_t> &input_sequence){
+list<uint8_t> bwe::move_to_front(list<uint8_t> &sequence){
     list<uint8_t> result;
     return result;
 }
 
-list<uint8_t> bwe::huffman_encode(list<uint8_t> &input_sequence){
+list<uint8_t> bwe::huffman_encode(list<uint8_t> &sequence){
     list<uint8_t> result;
     return result;
 }
 
-list<uint8_t> bwe::save_to_file(ofstream output){
-    list<uint8_t> result;
-    return result;
+void bwe::save_to_file(list<uint8_t> &output_sequence, ofstream &output){
+
 }
