7b79607f38238aeef5c5d42a772342afaadd8b23
soniap13 <sonia.pawluk13@gmail.com>
2020-05-25 18:00:39 +0200

    Read from file and write to file

--- BWE O(nlogn)/bwe-nlogn_tools.cpp
+++ BWE O(nlogn)/bwe-nlogn_tools.cpp
@@ -10,6 +10,7 @@ void bwe::encode(char * input_file, char * output_file, int block_size){
     ifstream input(input_file);
     if(!input.good())
         ERROR(input_file);
+    input >> noskipws;
     ofstream output(output_file);
     if(!output.good())
         ERROR(output_file);
@@ -18,7 +19,7 @@ void bwe::encode(char * input_file, char * output_file, int block_size){
     bool keep_goin = true;
 
     while(keep_goin){
-        keep_goin = read_block(block, input);
+        keep_goin = read_block(block, input, block_size);
         block = bwt(block);
         block = move_to_front(block);
         block = huffman_encode(block);
@@ -29,25 +30,32 @@ void bwe::encode(char * input_file, char * output_file, int block_size){
     output.close();
 }
 
-bool bwe::read_block(list<uint8_t> &input_sequence, ifstream &input){
-    return false;
+bool bwe::read_block(list<uint8_t> &input_sequence, ifstream &input, int block_size){
+    uint8_t elem;
+    for(int i = 0; i < block_size; i++){
+        if(input.eof()) return false;
+        input >> elem;
+        input_sequence.push_back(elem);
+    }
+    return true;
 }
 
 list<uint8_t> bwe::bwt(list<uint8_t> &sequence){
-    list<uint8_t> result;
-    return result;
+    return sequence;
 }
 
 list<uint8_t> bwe::move_to_front(list<uint8_t> &sequence){
-    list<uint8_t> result;
-    return result;
+    //list<uint8_t> result;
+    return sequence;
 }
 
 list<uint8_t> bwe::huffman_encode(list<uint8_t> &sequence){
-    list<uint8_t> result;
-    return result;
+    //list<uint8_t> result;
+    return sequence;
 }
 
 void bwe::save_to_file(list<uint8_t> &output_sequence, ofstream &output){
-
+    for(auto elem : output_sequence){
+        output << elem;
+    }
 }
